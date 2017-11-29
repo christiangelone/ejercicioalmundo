3ab1801306423852ed45cfcd978bc0ffc3a8dc1c
Christian Angelone <christiangelone@gmail.com>
2017-11-29 17:47:27 -0300

    Covers callCenter to have at least one operator

--- src/main/java/CallCenter.java
+++ src/main/java/CallCenter.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CallCenter {
 
+    public CallCenter(){
+        this.operators = new ArrayList<Operator>();
+        this.operators.add(new Operator());
+    }
+
+    private List<Operator> operators;
+
+    public List<Operator> getOperators() {
+        return operators;
+    }
 }
--- src/test/java/CallCenterTest.java
+++ src/test/java/CallCenterTest.java
@@ -1,6 +1,19 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-class CallCenterTest {
+public class CallCenterTest {
 
+    public CallCenter callCenter;
+
+    @Before
+    public void initialize(){
+        this.callCenter = new CallCenter();
+    }
+
+    @Test
+    public void shouldHaveAtLeastIOneOperator(){
+        Integer numberOfOperators = this.callCenter.getOperators().size();
+        assertTrue(numberOfOperators >= 1);
+    }
 }
