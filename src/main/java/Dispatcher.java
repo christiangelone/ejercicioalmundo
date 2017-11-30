8bcff25491e7efae161d9f05898ec2cc2efc1fc5
Christian Angelone <christiangelone@gmail.com>
2017-11-30 03:09:06 -0300

    Covers dispatcher to dispatch 10 calls

--- src/main/java/Dispatcher.java
+++ src/main/java/Dispatcher.java
@@ -5,11 +5,65 @@ public class Dispatcher {
 
     private final List<List<Attendant>> attendantsGroups;
 
+    private boolean verbose;
+
     public Dispatcher(List<List<Attendant>> attendantsGroups) {
+
         this.attendantsGroups = attendantsGroups;
+        this.verbose = false;
     }
 
     public List<List<Attendant>> getAttendantsGroups() {
         return this.attendantsGroups;
+    }
+
+    private boolean areAllAttendantsBusy(List<Attendant> attendants){
+        return attendants.stream()
+                .map(a -> a.isBusy())
+                .reduce((busy1,busy2) -> busy1 && busy2)
+                .orElse(true);
+    }
+
+    private boolean callsAnswered(List<Call> calls){
+        return calls.stream()
+                .map(call -> call.isAswered())
+                .reduce((answer1,answer2) -> answer1 && answer2)
+                .orElse(true);
+    }
+
+    private List<Attendant> findFirstFreeAttendants(){
+        for(List<Attendant> attendants: this.attendantsGroups){
+            if(!areAllAttendantsBusy(attendants))
+                return attendants;
+        }
+        return null;
+    }
+
+    private Attendant findAnyFreeAttendant(List<Attendant> attendants){
+        return attendants
+                .stream()
+                .filter(attendant -> !attendant.isBusy())
+                .findFirst()
+                .orElse(null);
+    }
+
+    private void dispatchCall(Call call){
+        List<Attendant> attendants = findFirstFreeAttendants();
+        if(attendants != null){
+            Attendant attendant = findAnyFreeAttendant(attendants);
+            attendant.answer(call);
+        }
+    }
+
+    public boolean dispatch(List<Call> calls) {
+        if(verbose) System.out.println("Dispatching calls...");
+        while(!callsAnswered(calls)){
+            calls.stream().forEach(call -> dispatchCall(call));
+        }
+        return true;
+    }
+
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
     }
 }
--- src/test/java/DispatcherTest.java
+++ src/test/java/DispatcherTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -10,16 +11,31 @@ public class DispatcherTest {
 
     public Dispatcher dispatcher;
 
-    @Before
-    public void initialize(){
+    @Test
+    public void shouldHaveAttendantsGroup(){
         List<List<Attendant>> attendantsGroups = new ArrayList<List<Attendant>>();
         this.dispatcher = new Dispatcher(attendantsGroups);
+        List<List<Attendant>> groups = this.dispatcher.getAttendantsGroups();
+        assertNotNull(groups);
     }
 
     @Test
-    public void shouldHaveAttendantsGroup(){
-        List<List<Attendant>> groups = this.dispatcher.getAttendantsGroups();
-        assertNotNull(groups);
+    public void shouldRespondTenConcurrentCalls(){
+        List<List<Attendant>> attendantsGroups = new ArrayList<List<Attendant>>();
+        List<Attendant> operators = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            operators.add(new Operator("Operator " + i ));
+        }
+
+        attendantsGroups.add(operators);
+        this.dispatcher = new Dispatcher(attendantsGroups);
+
+        List<Call> calls = new ArrayList<Call>();
+        for (int i = 0; i < 10; i++) {
+            calls.add(new Call(5,10));
+        }
+
+        assertTrue(this.dispatcher.dispatch(calls));
     }
 
 }
